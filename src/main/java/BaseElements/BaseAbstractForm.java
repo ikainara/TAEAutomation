a5f35c3e78a3f1bcf294f5db15ab9b66cf80da53
igor.kainara <igor.kainara@gmail.com>
2021-10-13 11:32:45 +0200

    Add Base Chrome test and Base page

---
+++ src/main/java/BaseElements/BaseAbstractForm.java
@@ -0,0 +1,12 @@
+package BaseElements;
+
+import Core.TAEBaseObject;
+import Core.TAEDriver;
+
+public class BaseAbstractForm extends TAEBaseObject {
+    TAEDriver driver;
+
+    public BaseAbstractForm(TAEDriver driver) {
+        this.driver = driver;
+    }
+}
